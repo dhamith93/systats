94d551114c2d672bcae0d5d7d6af521b31a49e98
Dhamith Hewamullage <hewamullage123@gmail.com>
2022-03-18 10:21:10 +0530

    Adding comments

--- exec/exec.go
+++ exec/exec.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 )
 
+// Execute execs the command with params returns output or error msg
 func Execute(command string, params ...string) string {
 	cmd := exec.Command(command, params...)
 	stdout, err := cmd.Output()
@@ -14,6 +15,7 @@ func Execute(command string, params ...string) string {
 	return string(stdout)
 }
 
+// ExecuteWithPipe execs commands with pipe returns output or error msg
 func ExecuteWithPipe(command string) string {
 	cmd := exec.Command("bash", "-c", command)
 	stdout, err := cmd.Output()
@@ -23,6 +25,7 @@ func ExecuteWithPipe(command string) string {
 	return string(stdout)
 }
 
+// ExecuteWithError execs the command with params returns output and error
 func ExecuteWithError(command string, params ...string) (string, error) {
 	cmd := exec.Command(command, params...)
 	stdout, err := cmd.Output()
@@ -32,6 +35,7 @@ func ExecuteWithError(command string, params ...string) (string, error) {
 	return string(stdout), nil
 }
 
+// ExecuteWithPipeAndError execs commands with pipe returns output and error
 func ExecuteWithPipeAndError(command string, params ...string) (string, error) {
 	cmd := exec.Command("bash", "-c", command)
 	stdout, err := cmd.Output()
@@ -41,6 +45,7 @@ func ExecuteWithPipeAndError(command string, params ...string) (string, error) {
 	return string(stdout), nil
 }
 
+// GetExecPath returns the execpath of binary
 func GetExecPath(cmd string) string {
 	result := Execute("whereis", cmd)
 	result = strings.TrimSpace(result)
